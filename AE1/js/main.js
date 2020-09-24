57c88c5eeb61b3ba962c3128a853865eb0fa9b19
juan_lujan <juanilloxyz@hotmail.com>
2020-09-24 10:55:44 +0200

    creo AE1.1

---
+++ AE1/js/main.js
@@ -0,0 +1,8 @@
+//ACTIVIDAD 1
+alert ("soy el primer script");
+let alertMessage="soy el primer script";
+alert (alertMessage);
+const  ALERT_MESSAGE=("soy el primer script") + " y estoy funcionando sobre" +navigator.appVersion;
+alert (ALERT_MESSAGE);
+
+
