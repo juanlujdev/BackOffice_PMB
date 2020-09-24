d81d28e3a7f961e55b8c26351f33598ac4448ed7
juan_lujan <juanilloxyz@hotmail.com>
2020-09-24 10:57:00 +0200

    Ejercicio AE1.2

--- AE1/js/main.js
+++ AE1/js/main.js
@@ -6,3 +6,16 @@ const  ALERT_MESSAGE=("soy el primer script") + " y estoy funcionando sobre" +na
 alert (ALERT_MESSAGE);
 
 
+//ACTIVIDAD 2.1
+let meses=[" enero", " febrero", " marzo", " abril", " mayo", " junio", " julio"," agosto"," septiembre"," octubre"," noviembre"," diciembre"]
+let mensaje="";
+for (let i=0;i<meses.length;i++){
+    mensaje=mensaje+meses[i];    
+}
+console.log(mensaje);
+
+
+//ACTIVIDAD 2.2
+
+let meses2=["enero", "febrero", "marzo", "abril", "mayo", "junio", "julio","agosto","septiembre","octubre","noviembre","diciembre"]
+console.table(meses2);
