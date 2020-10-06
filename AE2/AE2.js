1f2ef25239da4b01809e94ca102e225832d147ef
juan_lujan <juanilloxyz@hotmail.com>
2020-10-06 12:40:49 +0200

    subo AE2.1_a y AE2.1_b

--- AE2/AE2.js
+++ AE2/AE2.js
@@ -1,10 +1,20 @@
-//AE2_1
+//AE2_1.1 
 
 function deleteNumbers (valores){
         return typeof(valores) ==="string";
 }
-
 let valores = ["Ana",2,"Javi",5,"Roberto",7,"Vanessa",10];
 let newValores=valores.filter(deleteNumbers);
 console.log(newValores);
+
+
+//AE2_2.2
+function parNumbers(valores2){
+    if (typeof (valores2!=="string")){
+       return valores2%2==0;
+    }
+}
+let valores2 = ["Ana",2,"Javi",5,"Roberto",7,"Vanessa",10];
+let newValores2=valores2.filter(parNumbers);
+console.log(newValores2);
 
