18163c99dd9bff6278e13d07ac1722c4021bba7f
juan_lujan <juanilloxyz@hotmail.com>
2020-10-06 14:58:46 +0200

    realizo AE2_2

--- AE2/AE2.js
+++ AE2/AE2.js
@@ -18,3 +18,12 @@ let valores2 = ["Ana",2,"Javi",5,"Roberto",7,"Vanessa",10];
 let newValores2=valores2.filter(parNumbers);
 console.log(newValores2);
 
+
+//AE2_2
+function average(a,b){   
+    
+    return  a+b;
+}
+let dataArray=[7,4,1,9,10];
+console.log(dataArray.reduce(average,0)/dataArray.length);
+
