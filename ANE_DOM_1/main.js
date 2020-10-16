58c5a58ef7ab50cdaad1124cdd1a8b7996ee9215
juan_lujan <juanilloxyz@hotmail.com>
2020-10-16 11:05:38 +0200

    dejo solo la actividad ANE_DOM_1, tenia mezclados ejercicios

---
+++ ANE_DOM_1/main.js
@@ -0,0 +1,9 @@
+          
+            document.addEventListener('DOMContentLoaded',() => {
+                changeTitle();
+            });
+            const changeTitle= () => {
+                document.title = 'Ahora trabajo con DOM Dinamico';
+            }
+            
+       
