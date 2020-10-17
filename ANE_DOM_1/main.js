4eee7b243727489478208ae37ad1eff8a619c11a
juan_lujan <juanilloxyz@hotmail.com>
2020-10-17 17:56:23 +0200

    Realizo modificaciones en el index.html, creo un fichero aparte de css para el botón y realizo ejercicion 1,2,3

--- ANE_DOM_1/main.js
+++ ANE_DOM_1/main.js
@@ -1,9 +1,23 @@
           
             document.addEventListener('DOMContentLoaded',() => {
                 changeTitle();
+                addId();
+                halfFontSize();
             });
             const changeTitle= () => {
-                document.title = 'Ahora trabajo con DOM Dinamico';
+                document.title = 'Ahora trabajo con DOM Dinmico';
+            }
+            const addId=() => {
+                const add=document.getElementsByTagName('section');
+                [...add]
+                for(let i=1; i<add.length;i++){
+                    add[i].id=i;
+                }
+            }
+            const halfFontSize= () => {
+                const halfLetter=document.getElementsByTagName('p');
+                [...halfLetter].forEach(element => element.style.fontSize = '15px');           
             }
             
+
        
