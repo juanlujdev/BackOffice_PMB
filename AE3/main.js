674bd803808853521d0bd1db82a924774ed77950
juan_lujan <juanilloxyz@hotmail.com>
2020-10-25 12:58:40 +0100

    termino HTML parte1 y comienzo con js

---
+++ AE3/main.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded',() => {
+    validar();
+});
+    
+    const url;
+    const validar = () => {
+        const text=document.getElementById('cadena');
+        if(text<=0){
+
+        }
+        else{
+            url=text;
+        }
+    }
+
