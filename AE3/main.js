fb35d97d57dbe5977bf8fde84bd8a4b3096d5c81
juan_lujan <juanilloxyz@hotmail.com>
2020-10-30 12:00:54 +0100

    AE3 casi terminada

--- AE3/main.js
+++ AE3/main.js
@@ -1,60 +1,136 @@
 document.addEventListener('DOMContentLoaded',() => {
     validar();
-    aceptarFormulario();    
+    aceptarFormulario();
+    volverPaso1(); 
+    volverPaso2();
+    irPaso4();
+    terminar();
+    
 });    
     let text;
     let url;
+     
     const validar = () => {
         text=document.getElementById('cadena');
         //escucho si se teclea alguna tecla y si es asi voy a la funcion
         //PARA HACER CTRL+C Y CTRL+V NO LO DETECTA ASI Q LO QUE HARE ES ESCUCHAR SI SE HACER CLICK EN LA CAJA DE TEXTO Y LUEGO
         //COMPROBARE SI HAY MAS DE UN CARACTER EN LA CAJA, CON LOS IF QUE TENGO MAS ABAJO
-        text.addEventListener('blur', getEventType, false);
-        function getEventType(event) {
+        text.addEventListener('blur', getEventType);
+        function getEventType() {
             //guardo el valor que hay en la caja de texto en una variable, para comprobar en todo momento si esta borrado lo q teclee
             // o no, esta variable es solo de para comprobar
             const cont=document.getElementById('cadena').value;
             const botonContinuar=document.getElementById('continuar');
             //si hay mas de 0 caracteres habilito el boton y guardo en la var url la direccion
             if(cont.length>0){
-            botonContinuar.style.backgroundColor=" #1883ba";
-            botonContinuar.disabled=false;
-            botonContinuar.addEventListener('click',() => {                
-                //guardo en la url la direccion
-                url=document.getElementById('cadena').value; 
-                //es para comprobar que se guarda la direccion
-                console.log(url);
-                //oculto y muestro formulario y centrado
-                const formulario=document.getElementById('formulario');
-                const centrado=document.getElementById('centrado');
-                const paso1=document.getElementById('paso1');       
-                paso1.setAttribute('hidden','true');
-                formulario.removeAttribute('hidden');
-                centrado.setAttribute('hidden','true');
-                //LO HACE PERO ME DA ERROR PREGUNTAR COMO FUNCIONA EL BLUR (FOCO)
-                //modifico el id del boton atras  y lo habilito y continuar
-                //const buttonAtras=document.getElementById('atras');                
-                //buttonAtras.disabled='false';
-                //buttonAtras.style.backgroundColor="#1883ba";
-                //buttonAtras.id="formPrincipal";//vuelve al formulario inicial
-                botonContinuar.id="aceptarCampo";
-            });
+                botonContinuar.disabled=false;                    
+                botonContinuar.addEventListener('click',() => {                
+                    //guardo en la url la direccion
+                    url=document.getElementById('cadena').value; 
+                    //es para comprobar que se guarda la direccion
+                    //console.log(url);
+                    //oculto y muestro formulario y centrado
+                    const paso2=document.getElementById('paso2');
+                    const centrado=document.getElementById('centrado');
+                    const paso1=document.getElementById('paso1');
+                    paso1.setAttribute('hidden','true');
+                    paso2.removeAttribute('hidden');
+                    centrado.setAttribute('hidden','true');
+                    //botonContinuar.id="aceptarCampo";
+                });
+                //botonContinuar.disabled=true;
+                /*
+                    if(botonContinuar.disabled==true){
+                        botonContinuar.disabled=false;                    
+                        botonContinuar.addEventListener('click',() => {                
+                            //guardo en la url la direccion
+                            url=document.getElementById('cadena').value; 
+                            //es para comprobar que se guarda la direccion
+                            //console.log(url);
+                            //oculto y muestro formulario y centrado
+                            const paso2=document.getElementById('paso2');
+                            const centrado=document.getElementById('centrado');
+                            const paso1=document.getElementById('paso1');
+                            paso1.setAttribute('hidden','true');
+                            paso2.removeAttribute('hidden');
+                            centrado.setAttribute('hidden','true');
+                            botonContinuar.id="aceptarCampo";
+                    }   
+                        );
+                
+                }
+                else{
+                    otonContinuar.addEventListener('click',() => {                
+                        //guardo en la url la direccion
+                        url=document.getElementById('cadena').value; 
+                        //es para comprobar que se guarda la direccion
+                        //console.log(url);
+                        //oculto y muestro formulario y centrado
+                        const paso2=document.getElementById('paso2');
+                        const centrado=document.getElementById('centrado');
+                        const paso1=document.getElementById('paso1');
+                        paso1.setAttribute('hidden','true');
+                        paso2.removeAttribute('hidden');
+                        centrado.setAttribute('hidden','true');
+                        botonContinuar.id="aceptarCampo";
+                    });
+                } */                      
+            
+            
             }
             else{
                 botonContinuar.disabled=true;
-                botonContinuar.style.backgroundColor="grey";
+                //botonContinuar.style.backgroundColor="grey";
             }   
         }         
     }
-    const aceptarFormulario = () =>{
-        const aceptarCampo=document.getElementById('aceptarCampo');
-        if(aceptarCampo){
-            //aceptarCampo.onclick="validarFormulario()";
-            aceptarCampo.addEventListener('click',()=>{});
-        }
-        
-    }
+    const terminar= () =>{
+        const terminarButton=document.getElementById('terminar');
+        terminarButton.addEventListener('click',()=>{           
+            location.href='https://'+url;
+        })
 
+        //window.location="url";
+        //console.log("web nueva");
+    }
+    const irPaso4 = () => {
+        const continuar3=document.getElementById('continuar3');
+        continuar3.addEventListener('click',formularioTerminar);
+    }
+    const aceptarFormulario= () =>{
+        const continuar2=document.getElementById('continuar2');
+        continuar2.addEventListener('click',validarFormulario);
+    }
+    const volverPaso1= () => {
+        const atras2=document.getElementById('atras2');
+        atras2.addEventListener('click',volverInicio);
+    }
+    const volverPaso2= () => {
+        const atras3=document.getElementById('atras3');
+        atras3.addEventListener('click',volverFormulario);
+    }
+    const formularioTerminar= ()=>{
+        const paso3=document.getElementById('paso3');
+        const paso4=document.getElementById('paso4');
+        paso3.setAttribute('hidden','true');
+        paso4.removeAttribute('hidden');
+    }
+    const volverFormulario= ()=>{
+        const paso3=document.getElementById('paso3');
+        const paso2=document.getElementById('paso2');
+        const muestraError=document.getElementById('muestraError');
+        muestraError.setAttribute('hidden','true');
+        paso3.setAttribute('hidden','true');
+        paso2.removeAttribute('hidden');
+    }
+    const volverInicio= () =>{
+        const paso2=document.getElementById('paso2');
+        const centrado=document.getElementById('centrado');
+        const paso1=document.getElementById('paso1');
+        paso2.setAttribute('hidden','true');
+        paso1.removeAttribute('hidden');        
+        centrado.removeAttribute('hidden');
+    }
     const validarFormulario= () => {
         const name=document.getElementById('nombre').value;
         const date=document.getElementById('date').value;
@@ -66,12 +142,11 @@ document.addEventListener('DOMContentLoaded',() => {
         const direccionError=document.getElementById('errorDireccion');
         const cpError=document.getElementById('ErrorCp');
         const dateError=document.getElementById('ErrorDate');
-        const nameError=document.getElementById('ErrorName');
-        const caracteres=/^[a-zA-Z0-9]+$/;
+        const nameError=document.getElementById('ErrorName');        
+        const caracteres= /^[\sa-zA-Z0-9]+$/;
         const numeros=/^[0-9]+$/;
         let activacion=false;
         var message="Rellena los siguientes campos: \n";
-        var messageTrue="Tus datos son los siguientes: \t";
         if((name==="")||!(caracteres.test(name))){
             activacion=true;
             message+="Nombre \n";            
@@ -88,8 +163,8 @@ document.addEventListener('DOMContentLoaded',() => {
         else{
             dateError.setAttribute('hidden','true');
         }
-        //if(direccion != /^[a-zA-Z0-9]+$/)
-        if(!(caracteres.test(direccion))){
+        //const caracteres= /^[\sa-zA-Z0-9]+$/;
+        if((direccion!="")&&(!(caracteres.test(direccion)))){
             activacion=true;
             message+="Direccion \n";
             direccionError.removeAttribute('hidden');
@@ -97,7 +172,6 @@ document.addEventListener('DOMContentLoaded',() => {
         else{
             direccionError.setAttribute('hidden','true');
         }
-        //TENGO QUE HACER LA VALIDACION PARA QUE SEA SOLO NUMERICO ||(cp!=/^[0-9]+$/)
         if((cp==="")||!(numeros.test(cp))){
             activacion=true;
             message+="Codigo postal \n";            
@@ -120,14 +194,29 @@ document.addEventListener('DOMContentLoaded',() => {
                 muestraError.innerText=message; 
         }
         else{
-            messageTrue+="\n"+name+"\n"+date+"\n"+direccion+"\n"+cp+"\n"+provincia+"\n"+municipio+"\n";
-            const muestraError=document.getElementById('muestraError');
-                muestraError.removeAttribute('hidden');
-                muestraError.innerText=messageTrue;
-                muestraError.setAttribute("id","muestraOk");
-        }     
-
-    }     
+            const datos =new Object();
+            let salida="";
+            datos.nombre=name;
+            datos.fecha=date;
+            datos.direccion=direccion;
+            datos.cp=cp;
+            datos.provincia=provincia;
+            datos.municipio=municipio;
+            const datosJson=JSON.stringify(datos);
+            const miObjeto=JSON.parse(datosJson);
+            for(i in miObjeto){
+                salida +=  i+ "=" + miObjeto[i] + '\n';
+            }
+            const paso3=document.getElementById('paso3');
+            const paso2=document.getElementById('paso2');
+            const muestraOk=document.getElementById('muestraOk');
+            paso2.setAttribute('hidden','true');
+            paso3.removeAttribute('hidden');
+            muestraOk.innerText=salida;
             
+            
+        }
+    }     
+
     
 
