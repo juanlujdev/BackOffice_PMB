e0b5c1350a631bfdd4a1105c7509b4573007c10f
juan_lujan <juanilloxyz@hotmail.com>
2020-12-19 21:00:51 +0100

    ANE7 Finalizada a falta de limpiar codigo

--- ane7/src/components/ListUser.js
+++ ane7/src/components/ListUser.js
@@ -1,20 +1,28 @@
 import * as React from 'react';
-import data from '../users.json';
+import {Fragment} from 'react';
 
 export default class ListUser extends React.Component {
-    render() {
+    render() {//render() significa pintar por pantalla, quiero que me saque por pantalla la imagen y
+        //Nombre: y como quiero que se llame la propiedad (en este caso name, puede ser como quieras
         return (
-            <div>
-                {data.map((dataJson) => {
-                    //if (dataJson.surname === 'Salvador')
-                        return (
-                            <div>
-                                <p>{dataJson.name} {dataJson.surname}</p>
-                                <p>{dataJson.dateOfBirth}</p>
-                            </div>
-                        );
-                })}
-            </div>
+            <Fragment>
+                <div className={'dataUser'}>
+                    <div className={'picture'}>
+                        <img
+                            src={'https://hotmart.s3.amazonaws.com/product_contents/3f4c8a14-9928-4c77-8f14-605b5beb93db/SzilagyiZoltanAvatarSmall300x300.jpg'}>
+                        </img>
+                    </div>
+                    {/*como quiero pasarle los datos al constructor, quiero que salga en la lista
+                    Nombre: el nombre, Apellidos: apellidos, fecha nacimiento: nac. Son los nombres que quiero
+                    ponerle asi que en props (que hereda de React.Component) tendrá
+                    */}
+                    <div>
+                        <p>Nombre: {this.props.name}</p>
+                        <p>Apellidos: {this.props.surname} </p>
+                        <p>Fecha de nacimiento: {this.props.dateOfBirth}</p>
+                    </div>
+                </div>
+            </Fragment>
         );
     }
 }
