df7a0036175bc82898cd0f8d45cdbec5625bcbdb
juan_lujan <juanilloxyz@hotmail.com>
2021-01-04 18:50:05 +0100

    PE1 modifico barra menu

--- pmb-backoffice/src/App.js
+++ pmb-backoffice/src/App.js
@@ -4,44 +4,44 @@ import {Fragment} from "react";
 import 'primereact/resources/themes/saga-blue/theme.css';
 import 'primereact/resources/primereact.min.css';
 import 'primeicons/primeicons.css';
-import {Menu} from 'primereact/menu';
+import { TabMenu } from 'primereact/tabmenu';
 import {Switch, Route} from "react-router-dom";
 import {UsersView} from "./UsersView";
 import {BetsView} from "./BetsView";
 import {EventsView} from "./EventsView";
 import {ReportView} from "./ReportView";
 
-let items = [
-    {
-        label: 'Usuario', icon: 'pi pi-user', command: () => {
-            window.location = '/users';
-        }
-    },
-    {
-        label: 'Apuestas', icon: 'pi pi-money-bill', command: () => {
-            window.location = '/bets';
-        }
-    },
-    {
-        label: 'Eventos', icon: 'pi pi-ticket', command: () => {
-            window.location = '/events';
-        }
-    },
-    {
-        label: 'Informes', icon: 'pi pi-chart-line', command: () => {
-            window.location = '/reports';
-        }
-    }
-];
+const TabMenuDemo = () => {
+
+    const items = [
+        {label: 'Usuarios',  icon: 'pi pi-user', command: () => {
+                window.location = '/users';}},
+        {label: 'Apuestas', icon: 'pi pi-money-bill', command: () => {
+                window.location = '/bets';
+            }},
+        {label: 'Eventos', icon: 'pi pi-ticket', command: () => {
+                window.location = '/events';
+            }},
+        {label: 'Informes', icon: 'pi pi-chart-line', command: () => {
+                window.location = '/reports';
+            }}
+    ];
+
+    return (
+        <div>
+            <div className="card">
+                <TabMenu model={items} />
+            </div>
+        </div>
+    );
+}
 export default class App extends React.Component {
 
     render() {
         return (
             <div className="App">
                 <Fragment>
-                    <div>
-                        <Menu model={items}/>
-                    </div>
+                   <TabMenuDemo/>
                     <div className={'display'}>
                         <Switch>
                             <Route path={'/users'}>
