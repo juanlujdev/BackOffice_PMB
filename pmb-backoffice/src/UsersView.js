0bd1c1d1cf6e47c97c0180e981aa40fb82e5d1ec
juan_lujan <juanilloxyz@hotmail.com>
2021-01-04 19:46:25 +0100

    PE1_USER FRONTEND le doy estilo e input y button

--- pmb-backoffice/src/UsersView.js
+++ pmb-backoffice/src/UsersView.js
@@ -1,9 +1,17 @@
 import * as React from "react";
+import {InputText} from "primereact/inputtext";
+import { Button } from "primereact/button";
 
 export class UsersView extends React.Component {
     render() {
         return (
-            <p>pagina User</p>
+
+            <div className={'fiter'}>
+                <InputText placeholder="Nombre"/>
+                <InputText placeholder="Apellidos"/>
+                <InputText placeholder="Email"/>
+                <Button label="Submit" icon="pi pi-check" />
+            </div>
         );
     }
 }
