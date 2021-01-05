cf3629c1385abd013e3c5361cb05a63dcd5b306a
juan_lujan <juanilloxyz@hotmail.com>
2021-01-05 17:25:42 +0100

    creo Frontend Usuario

--- pmb-backoffice/src/UsersView.js
+++ pmb-backoffice/src/UsersView.js
@@ -1,17 +1,33 @@
 import * as React from "react";
 import {InputText} from "primereact/inputtext";
-import { Button } from "primereact/button";
+import {Button} from "primereact/button";
+import {DataTable} from "primereact/datatable";
+import {Column} from "primereact/column";
+import 'primereact/resources/themes/saga-blue/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
+import {Fragment} from "react";
 
 export class UsersView extends React.Component {
     render() {
         return (
+            <Fragment>
+                <div className={'fiter'}>
+                    <InputText placeholder="Nombre"/>
+                    <InputText placeholder="Apellidos"/>
+                    <InputText placeholder="Email"/>
+                    <Button label="Buscar" icon="pi pi-check"/>
+                </div>
+                <div>
+                    <DataTable>
+                        <Column field="EmailId" header="EmailId"></Column>
+                        <Column field="Nombre" header="Nombre"></Column>
+                        <Column field="Apellidos" header="Apellidos"></Column>
+                        <Column field="Edad" header="Edad"></Column>
+                    </DataTable>
+                </div>
+            </Fragment>
 
-            <div className={'fiter'}>
-                <InputText placeholder="Nombre"/>
-                <InputText placeholder="Apellidos"/>
-                <InputText placeholder="Email"/>
-                <Button label="Submit" icon="pi pi-check" />
-            </div>
         );
     }
 }
