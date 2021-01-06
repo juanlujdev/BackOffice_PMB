6e17e76ac8396a2d2cbf5a9b2f9164e217083267
juan_lujan <juanilloxyz@hotmail.com>
2021-01-06 11:02:39 +0100

    PE1_Bets creo el filtro para las apuestas

--- pmb-backoffice/src/BetsView.js
+++ pmb-backoffice/src/BetsView.js
@@ -1,9 +1,37 @@
 import * as React from "react";
+import {InputText} from "primereact/inputtext";
+import {Button} from "primereact/button";
+import {DataTable} from "primereact/datatable";
+import {Column} from "primereact/column";
+import 'primereact/resources/themes/saga-blue/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
+import {Fragment} from "react";
 
 export class BetsView extends React.Component {
     render() {
         return (
-            <p>pagina Apuestas</p>
+            <Fragment>
+                <div className={'fiter'}>
+                    <InputText placeholder="Email"/>
+                    <InputText placeholder="Mercado"/>
+                    <InputText placeholder="Evento"/>
+                    <Button label="Buscar" icon="pi pi-check"/>
+                </div>
+                <div>
+                    <DataTable>
+                        <Column field="ApuestaID" header="ApuestaID"></Column>
+                        <Column field="MercadoOverUnder" header="Mercado"></Column>
+                        <Column field="TipoOverUnder" header="Tipo"></Column>
+                        <Column field="Cuota" header="Cuota"></Column>
+                        <Column field="DineroApostado" header="Dinero"></Column>
+                        <Column field="Fecha" header="Fecha"></Column>
+                        <Column field="UsuarioId" header="Usuario"></Column>
+                        <Column field="MercadoId" header="Mercado"></Column>
+                    </DataTable>
+                </div>
+            </Fragment>
+
         );
     }
 }
