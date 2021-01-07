327dac97385e11710ed28a0b5004a1b090438ce1
juan_lujan <juanilloxyz@hotmail.com>
2021-01-07 11:53:49 +0100

    PE1 saco todas los usuarios

--- pmb-backoffice/src/UsersView.js
+++ pmb-backoffice/src/UsersView.js
@@ -7,27 +7,46 @@ import 'primereact/resources/themes/saga-blue/theme.css';
 import 'primereact/resources/primereact.min.css';
 import 'primeicons/primeicons.css';
 import {Fragment} from "react";
+import axios from "axios";
 
 export class UsersView extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            userInfoData: []
+        }
+        this.getInfoUser();
+    }
+
     render() {
         return (
             <Fragment>
                 <div className={'fiter'}>
-                    <InputText placeholder="Nombre"/>
+                    <InputText  placeholder="Nombre"/>
                     <InputText placeholder="Apellidos"/>
                     <InputText placeholder="Email"/>
                     <Button label="Buscar" icon="pi pi-check"/>
                 </div>
                 <div>
-                    <DataTable>
-                        <Column field="EmailId" header="EmailId"></Column>
+                    <DataTable value={this.state.userInfoData}>
+                        <Column field="UsuarioId" header="EmailId"></Column>
                         <Column field="Nombre" header="Nombre"></Column>
-                        <Column field="Apellidos" header="Apellidos"></Column>
-                        <Column field="Edad" header="Edad"></Column>
+                        <Column field="Apellido" header="Apellidos"></Column>
+                        <Column field="edad" header="Edad"></Column>
                     </DataTable>
                 </div>
             </Fragment>
 
         );
+    }
+
+    /*metodo que traigo con axios los datos de la api de usuarios, me lo guarda en resultRequest
+     y me actualizas con el setState en userInfoData que sea igual a resultRequest*/
+    getInfoUser = () => {
+        axios.get('https://localhost:44301/api/usuarios').then((resultRequest) => {
+            this.setState({userInfoData: resultRequest.data});
+            console.log(this.state.userInfoData);
+        })
     }
 }
