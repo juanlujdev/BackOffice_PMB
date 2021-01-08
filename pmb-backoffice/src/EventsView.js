020ea9500a557b433f2117386acca6fe82915368
juan_lujan <juanilloxyz@hotmail.com>
2021-01-08 16:46:42 +0100

    PE1 utilidad hidden hecha

--- pmb-backoffice/src/EventsView.js
+++ pmb-backoffice/src/EventsView.js
@@ -1,20 +1,38 @@
 import * as React from "react";
+import {Fragment} from "react";
+
 import {InputText} from "primereact/inputtext";
 import {Button} from "primereact/button";
 import {DataTable} from "primereact/datatable";
 import {Column} from "primereact/column";
+
 import 'primereact/resources/themes/saga-blue/theme.css';
 import 'primereact/resources/primereact.min.css';
 import 'primeicons/primeicons.css';
-import {Fragment} from "react";
+
 
 export class EventsView extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            stateInputLocal: false,
+            stateInputVisitante: false,
+            stateInputFecha: false,
+            local: '',
+            visitante: '',
+            fecha: ''
+        }
+    }
+
     render() {
         return (
             <Fragment>
                 <div className={'fiter'}>
-                    <InputText placeholder="Titulo"/>
-                    <InputText placeholder="Fecha"/>
+                    <InputText onChange={this.getByLocal} disabled={this.state.stateInputLocal} placeholder="Local"/>
+                    <InputText onChange={this.getByVisitante} disabled={this.state.stateInputVisitante}
+                               placeholder="Visitante"/>
+                    <InputText onChange={this.getByDate} disabled={this.state.stateInputFecha} placeholder="Fecha"/>
                     <Button label="Buscar" icon="pi pi-check"/>
                 </div>
                 <div>
@@ -28,5 +46,39 @@ export class EventsView extends React.Component {
             </Fragment>
 
         );
+    }
+
+    getByLocal = (localInput) => {
+        this.setState({local: localInput.target.value},
+            () => {
+                if (this.state.local.length > 0) {
+                    this.setState({stateInputVisitante: true, stateInputFecha: true})
+                } else {
+                    this.setState({stateInputVisitante: false, stateInputFecha: false})
+                }
+            }
+        );
+    }
+    getByVisitante = (visitantInput) => {
+        this.setState({visitante: visitantInput.target.value},
+            () => {
+                if (this.state.visitante.length > 0) {
+                    this.setState({stateInputLocal: true, stateInputFecha: true})
+                } else {
+                    this.setState({stateInputLocal: false, stateInputFecha: false})
+                }
+            }
+        );
+    }
+    getByDate = (dateInput) => {
+        this.setState({fecha: dateInput.target.value},
+            () => {
+                if (this.state.fecha.length > 0) {
+                    this.setState({stateInputLocal: true, stateInputVisitante: true});
+                } else {
+                    this.setState({stateInputLocal: false, stateInputVisitante: false});
+                }
+            }
+        )
     }
 }
