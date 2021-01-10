704202dcd9b64d21518831ca1566b810fa670bb9
juan_lujan <juanilloxyz@hotmail.com>
2021-01-10 19:31:16 +0100

    ticket

--- pmb-backoffice/src/ReportView.js
+++ pmb-backoffice/src/ReportView.js
@@ -3,33 +3,53 @@ import {Chart} from 'primereact/chart';
 import {Fragment} from "react";
 
 export class ReportView extends React.Component {
-    const
-    basicData = {
-        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-        datasets: [
-            {
-                label: 'My First dataset',
-                backgroundColor: '#42A5F5',
-                data: [65, 59, 80, 81, 56, 55, 40]
-            },
-            {
-                label: 'My Second dataset',
-                backgroundColor: '#FFA726',
-                data: [28, 48, 40, 19, 86, 27, 90]
-            }
-        ]
-    };
+
     render() {
+        const
+            basicData = {
+                labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+                datasets: [
+                    {
+                        label: 'My First dataset',
+                        backgroundColor: '#42A5F5',
+                        data: [65, 59, 80, 81, 56, 55, 40]
+                    },
+                    {
+                        label: 'My Second dataset',
+                        backgroundColor: '#FFA726',
+                        data: [28, 48, 40, 19, 86, 27, 90]
+                    }
+                ]
+            };
+        let basicOptions = {
+            legend: {
+                labels: {
+                    fontColor: '#495057'
+                }
+            },
+            scales: {
+                xAxes: [{
+                    ticks: {
+                        fontColor: '#495057'
+                    }
+                }],
+                yAxes: [{
+                    ticks: {
+                        fontColor: '#495057'
+                    }
+                }]
+            }
+        }
         return (
             <Fragment>
                 <div className="card">
                     <h5>Vertical</h5>
-                    <Chart type="bar" data={basicData} options={basicOptions} />
+                    <Chart type="bar" data={basicData} options={basicOptions}/>
                 </div>
 
                 <div className="card">
                     <h5>Horizontal</h5>
-                    <Chart type="horizontalBar" data={basicData} options={basicOptions} />
+                    <Chart type="horizontalBar" data={basicData} options={basicOptions}/>
                 </div>
             </Fragment>
         );
